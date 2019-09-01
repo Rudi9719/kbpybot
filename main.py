cbe9ad0657456454a585378010b78a15d1b5c807
Gregory Rudolph <rudi@barnabas.localdomain>
2019-09-01 18:47:27 -0400

    Silencing Pycharm

--- main.py
+++ main.py
@@ -12,32 +12,36 @@ active_teams = []
 bt = Private("private")
 active_teams.append(bt)
 
+team_found = False  # For the loop below
+
+
+# Process the kbmsg from Keybase's API
+def process_kbmsg(message, sender, team, channel):
+    if sender != bot_name:
+        if isinstance(channel, str):
+            if "@{}".format(bot_name) in message:
+                global team_found
+                team_found = False
+                print("{} said {} in @{}#{}".format(sender, message, team, channel))
+                for active_team in active_teams:
+                    if team == active_team.team_name:
+                        team_found = True
+                        active_team.handle(message, sender, channel)
+                if not team_found:
+                    bt.send_message("I don't belong here.", team=team, channel=channel)
+
+        else:
+            # Is a PM
+            print("{} said {}".format(sender, message))
+
 
 class Handler:
     async def __call__(self, bot, event):
         if event.msg.content.type != ContentType.TEXT:
             return
         else:
-            self.process_kbmsg(event.msg.content.text.body, event.msg.sender.username,
-                               event.msg.channel.name, event.msg.channel.topic_name)
-
-    def process_kbmsg(self, message, sender, team, channel):
-        if sender != bot_name:
-            if isinstance(channel, str):
-                if "@{}".format(bot_name) in message:
-                    global team_found
-                    team_found = False
-                    print("{} said {} in @{}#{}".format(sender, message, team, channel))
-                    for active_team in active_teams:
-                        if team == active_team.team_name:
-                            team_found = True
-                            active_team.handle(message, sender, channel)
-                    if not team_found:
-                        bt.send_message("I don't belong here.", team=team, channel=channel)
-
-            else:
-                # Is a PM
-                print("{} said {}".format(sender, message))
+            process_kbmsg(event.msg.content.text.body, event.msg.sender.username,
+                          event.msg.channel.name, event.msg.channel.topic_name)
 
 
 listen_options = {

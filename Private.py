99379b967ba47a0a2b7c638c299bfb6217a02684
Gregory Rudolph <rudi@barnabas.localdomain>
2019-09-01 18:43:38 -0400

    Cleaning up signature

--- Private.py
+++ Private.py
@@ -1,9 +1,13 @@
 from BaseTeam import BaseTeam
 
+
 class Private(BaseTeam):
 
     def __init__(self, team_name):
         super().__init__(team_name)
 
-    def handle(self, channel, message, sender):
-        self.random_message(sender)
+    def handle(self, message, sender, team=None, channel=None):
+        if sender is not None:
+            self.random_message(sender)
+        else:
+            super().handle(message=message, sender=sender, team=self.team_name, channel=channel)
